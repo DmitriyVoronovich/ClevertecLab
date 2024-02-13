3ae53c527ed2b73e12670f0616315ae45edaeec1
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2024-02-13 14:14:42 +0300

    init: start sprint-1

---
+++ src/data/data.ts
@@ -0,0 +1,8 @@
+import heard from '../accets/image/heard.svg';
+import cal from '../accets/image/cal.svg';
+import prof from '../accets/image/prof.svg';
+export const data = [
+    {id: 1, title: 'Тренировки', icon: heard, description: 'Расписать тренировки'},
+    {id: 2, title: 'Календарь', icon: cal, description: 'Назначить календарь'},
+    {id: 3, title: 'Профиль', icon: prof, description: 'Заполнить профиль'},
+]
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,34 +1,16 @@
-import React, { useState } from 'react';
-
-import reactLogo from '/react.svg';
-import viteLogo from '/vite.svg';
-import tsLogo from '/ts.svg';
+import React from 'react';
 import './main-page.css';
+import {Header} from "@pages/main-page/header/Header.tsx";
+import {MainSection} from "@pages/main-page/main-section/MainSection.tsx";
+import fon from "../../accets/main_page_light.png";
 
 export const MainPage: React.FC = () => {
-    const [count, setCount] = useState(0);
 
     return (
-        <>
-            <div>
-                <a href='https://vitejs.dev' target='_blank'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank'>
-                    <img src={reactLogo} className='logo react' alt='React logo' />
-                </a>
-                <a href='https://www.typescriptlang.org/' target='_blank'>
-                    <img src={tsLogo} className='logo' alt='TS logo' />
-                </a>
-            </div>
-            <h1>Vite + React + TS</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/pages/main-page.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-        </>
+        <div className={'main_page_container'} style={{ backgroundImage: `url(${fon})`,
+            backgroundRepeat: 'no-repeat', backgroundSize: '100%'}}>
+            <Header/>
+            <MainSection/>
+        </div>
     );
 };
